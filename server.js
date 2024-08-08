b74f5a64528b95700394b289e3e9c0d92046481d
Tair <tairjanametov@gmail.com>
2024-08-09 04:21:28 +0500

    add route for get data from users sql

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const connection = require("./connect");
 const http = require("http");
 const app = express();
+const route = express.Router();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const routerUsers = route.get("/users", (req, res, next) => {
+  connection.query("SELECT * FROM users", (err, result) => {
+    if (!err) {
+      res.status(200).json(result);
+    } else {
+      res.status(503).json({ message: "Error on server" });
+    }
+  });
+});
+app.use("/users", routerUsers);
 http.createServer(app);
 app.listen(8080, () => {
   console.log("works");
