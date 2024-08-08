5f3312356d1cb1b1637e0478f4f0ff5a8353524f
Tair <tairjanametov@gmail.com>
2024-08-09 04:29:10 +0500

    add file for route users and separated route from server and app

---
+++ routes/getUsers.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const connection = require("../connect");
+const route = express.Router();
+
+route.get("/users", (req, res, next) => {
+  connection.query("SELECT * FROM users", (err, result) => {
+    if (!err) {
+      res.status(200).json(result);
+    } else {
+      res.status(503).json({ message: "problems with server" });
+    }
+  });
+});
+module.exports = route;
--- server.js
+++ server.js
@@ -2,19 +2,11 @@ const express = require("express");
 const connection = require("./connect");
 const http = require("http");
 const app = express();
-const route = express.Router();
+const getUsers = require("./routes/getUsers");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const routerUsers = route.get("/users", (req, res, next) => {
-  connection.query("SELECT * FROM users", (err, result) => {
-    if (!err) {
-      res.status(200).json(result);
-    } else {
-      res.status(503).json({ message: "Error on server" });
-    }
-  });
-});
-app.use("/users", routerUsers);
+
+app.use("/users", getUsers);
 http.createServer(app);
 app.listen(8080, () => {
   console.log("works");
